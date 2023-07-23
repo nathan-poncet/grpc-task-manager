45622617c4f9196c1c96f955293b8406ad7c4de1
nathan <nathan.poncet.pro@gmail.com>
2023-07-23 23:20:00 +0200

    ✨ add car api

--- api/race-api/src/race/race.module.ts
+++ api/race-api/src/race/race.module.ts
@@ -4,11 +4,13 @@ import { Race, RaceSchema } from './entity/race.schema';
 import { RaceService } from './race.service';
 import { RaceController } from './race.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Race.name, schema: RaceSchema }]),
     AuthModule,
+    UserModule,
   ],
   providers: [RaceService],
   controllers: [RaceController],
